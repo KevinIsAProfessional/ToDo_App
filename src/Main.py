062123d2fb4871cf28a29a9122743acd8f481526
Kevin Christensen <k.j.christensen93@gmail.com>
2023-10-24 12:44:46 -0600

    feat: Add new task input logic
    Adds logic for adding a new task to the list. User can now enter task
    text, select task type, and accept the new task, which then adds the
    task to the Treeview

--- src/Main.py
+++ src/Main.py
@@ -19,28 +19,50 @@ def add_tree(parent_frame):
     tree.column('task', width=320, anchor=tk.W)
     tree.grid(row=0, column=0, sticky='nsew')
 
+    return tree
 
-def add_input(parent_frame):
+
+def add_input(parent_frame, tree_frame):
+    """ Adds a frame to the bottom of the window which accepts user
+        input to add new tasks to the Treeview frame"""
+
     # FIXME: width and height are hardcoded - change to dynamic later
-    input_form = tk.Frame(parent_frame, height=100, width=400)
+    # height=100, width=600)
+    input_form = tk.Frame(parent_frame)
 
-    placeholder = tk.Label(input_form, text="New ToDo")
-    placeholder.grid(row=0, column=0, sticky='w')
+    placeholder = tk.Label(input_form, text="New task")
+    placeholder.grid(row=0, column=0)
 
+    # Add entry field for task text
     input_field = tk.Entry(input_form)
-    input_field.grid(row=0, column=2, sticky='w')
+    input_field.grid(row=0, column=1)
 
-    input_button = tk.Button(input_form, text='accept')
-    input_button.grid(row=0, column=2, sticky='e')
+    # Add dropdown selection for task type
+    task_types = ["TODO", "DONE", "URGE"]
+    input_type = ttk.Combobox(input_form, values=task_types)
+    input_type.set("Task type")
+    input_type.grid(row=0, column=2)
 
-    input_form.grid(row=1, column=0, sticky='s')
+    # Add button to accept new tasks and add them to the Treeview
+    def on_press(): return add_task(tree_frame, input_field, input_type)
+    input_accept = tk.Button(input_form, text='accept', command=on_press)
+    input_accept.grid(row=0, column=3)
+
+    input_form.grid(row=1, column=0, sticky='nsew')
+
+
+def add_task(tree_frame, input_field, input_type):
+    task_type = input_type.get()
+    task_text = input_field.get()
+    task = (task_type, task_text)
+    tree_frame.insert('', tk.END, values=task)
 
 
 def main():
     root = tk.Tk()
     root.title("Git er dun ya lazy shit")
 
-    window_width = 400
+    window_width = 520
     window_height = 255
 
     # Get screen dimension
@@ -58,10 +80,10 @@ def main():
     root.resizable(False, False)
 
     # add the task list to the frame
-    add_tree(root)
+    tree = add_tree(root)
 
     # Add the task adding form to the frame
-    add_input(root)
+    add_input(root, tree)
 
     root.mainloop()
 
